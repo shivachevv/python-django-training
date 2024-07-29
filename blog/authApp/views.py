05e0c0cdeeb786aef12629c2937947ab8f7c2a91
nikola-shivachev-mm <nikola.shivachev@mentormate.com>
2024-07-29 18:15:28 +0300

    added auth logic and navigation

--- blog/authApp/views.py
+++ blog/authApp/views.py
@@ -1,8 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
 
-def login(request):
-    return HttpResponse('this is a login')
+def login_controller(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "auth/login.html", {"form": form})
+
+
+def register_controller(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "auth/register.html", {"form": form})
+
+
+def logout_controller(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("home")
