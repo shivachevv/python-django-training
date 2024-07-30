9939c93cd4e192c428fb6e2c1b3178cad4512268
nikola-shivachev-mm <nikola.shivachev@mentormate.com>
2024-07-30 14:12:31 +0300

    blogpost list service

--- blog/postApp/services.py
+++ blog/postApp/services.py
@@ -1,15 +1,23 @@
 from django.http import HttpRequest
 from .models import BlogPost, BlogPostReaction, Comment, Reaction
+from .utils import BlogPostUtils
 
 
 def get_posts(user_id: int):
+    blogposts = BlogPost.objects.all().filter(user_id=user_id)
 
-    blogs = BlogPost.objects.all().filter(
-        user_id=user_id).select_related('reactions')
-    print('blogs', blogs.values())
-    # print('service', request.user.id)
-    return [
-        {
-            "title": "TITLE 1"
-        }
-    ]
+    result = []
+
+    for blogpost in blogposts:
+        reactions = blogpost.reactions.all().values()
+        comments = blogpost.comments.all().values()
+
+        result.append({
+            'id': blogpost.id,
+            'title': blogpost.title,
+            'author': f'{blogpost.user.first_name} {blogpost.user.last_name}',
+            'reactions': BlogPostUtils.prettify_reactions(reactions),
+            'comments': len(comments),
+        })
+
+    return result
